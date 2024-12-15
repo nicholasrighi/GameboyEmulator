013defcb32bceb741a582afa79990333f3b7b125
Nick Righi <nicholasrighi@gmail.com>
2024-12-14 23:19:57 -0800

    memory: Add method to set byte of memory
    Signed-off-by: Nick Righi <nicholasrighi@gmail.com>

--- src/memory.rs
+++ src/memory.rs
@@ -11,4 +11,8 @@ impl Memory {
     pub fn get_data(self: &Self, address: u16) -> u8 {
         self.data[usize::from(address)]
     }
+
+    pub fn set_byte(self: &mut Self, address: u16, data: u8) {
+        self.data[usize::from(address)] = data;
+    }
 }
